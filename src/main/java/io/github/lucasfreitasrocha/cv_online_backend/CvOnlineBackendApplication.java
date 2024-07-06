43f79c2e3deffd676b04af8e569899fb68bd136f
Lucas Freitas da Rocha <rochadefreitaslucas@gmail.com>
2024-07-06 11:20:42 -0300

    init

---
+++ src/main/java/io/github/lucasfreitasrocha/cv_online_backend/CvOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasfreitasrocha.cv_online_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvOnlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvOnlineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lucasfreitasrocha/cv_online_backend/CvOnlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasfreitasrocha.cv_online_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvOnlineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
